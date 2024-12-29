d5f7410e0f7580053b042761c409dc0840394891
Erwdy <print_0@126.com>
2024-12-29 15:14:40 +0800

    install program update

--- install.py
+++ install.py
@@ -1,6 +1,25 @@
 inf=open('../script.js','r',encoding='utf-8').read()
 if inf.find('/*mod_start*/')==-1:
-    open('../script.js','w',encoding='utf-8').write(inf+r"/*mod_start*/;window.document.title+='mod版本';try{var modList=require('fs').readFileSync('./mod.txt').toString();modList=modList.split(/[\s\n]/);for(var i=0;i<modList.length;i++){modList[i]=modList[i].split('|');}for(var i=0;i<modList.length;i+=2){if(modList[i][1]=='on'){try{var script=require('fs').readFileSync('./mod/'+modList[i][0]+'/script.js').toString();eval(script);}catch(e){alert('error:mod:'+modList[i][0]+':'+e.toString());}}}}catch(e){alert('error:'+e.toString());};/*mod_end*/")
+    open('../script.js','w',encoding='utf-8').write(inf+'''/*mod_start*/
+;window.document.title+='mod版本';
+try{
+    var modList=require('fs').readFileSync('./mod.txt').toString();
+    modList=modList.split(/[\s\\n]/);
+    for(var i=0;i<modList.length;i++){
+        modList[i]=modList[i].split('|');
+    }for(
+        var i=0;i<modList.length;i+=2
+    ){
+        if(modList[i][1]=='on'){
+            var script=require('fs').readFileSync('./mod/'+modList[i][0]+'/script.js').toString();
+            var s=document.createElement('script');
+            document.body.appendChild(s);
+            s.appendChild(document.createTextNode(`try{;${script};}catch(e){alert('error:mod:${modList[i][0]}:'+e.toString());}`));
+            // eval(script);
+        }
+    }
+}catch(e){alert('error:'+e.toString());};
+/*mod_end*/''')
 #if inf[-11:]!='error\')}});':
 #    out=inf+"\nrequire('fs').readFile('./mod/script.js',function(e,data){if(!e){eval(data.toString())}else{alert('mod error')}});"
 
