6dc02b059f5f367ea73187ded0507457335057b2
Erwdy <print_0@126.com>
2024-12-29 16:06:10 +0800

    GC developer support

---
+++ rootFramework.js
@@ -0,0 +1,52 @@
+/*mod_start*//*this is the mod framework for gc game developer*/
+;window.document.title+='mod版本';
+try{
+    var fs=require('fs');
+    var isFirstRun=!fs.existsSync('./mod.txt');
+    if(isFirstRun){
+        var modList=[]
+        fs.writeFileSync('./mod.txt','')
+    }else{
+        var modList=fs.readFileSync('./mod.txt').toString();
+        modList=modList.split(/[\s\n]/);
+        for(var i=0;i<modList.length;i++){
+            if(modList[i].includes('|')){
+                modList[i]=modList[i].split('|');
+            }else if(modList[i].includes('/')){
+                modList[i]=modList[i].split('/');
+            }else if(modList[i].includes(':')){
+                modList[i]=modList[i].split(':');
+            }
+        }
+    }
+    var codeLS={}
+    var workshopPath=`../../workshop/content/${3265880}`;//game appid
+    if(fs.existsSync(workshopPath)){
+        var directories = fs.readdirSync(workshopPath);
+        for(var i in directories){
+            if(!fs.lstatSync(`${workshopPath}/${directories[i]}`).isDirectory()){continue;}
+            var two=fs.readdirSync(`${workshopPath}/${directories[i]}`)
+            for(var j in two){
+                if(two[j].includes('ScriptNameFlag_')){
+                    var codeName=two[j].slice(15);
+                    if(fs.existsSync(`${workshopPath}/${directories[i]}/script.js`)){
+                        codeLS[codeName]=`${workshopPath}/${directories[i]}/script.js`;
+                        if(isFirstRun){
+                            fs.appendFileSync('./mod.txt',`${codeName}:on\n`);
+                            modList.push([codeName,'on']);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for(var i=0;i<modList.length;i+=2){
+        if(modList[i][1]=='on'){
+            var script=require('fs').readFileSync(codeLS[modList[i][0]]?codeLS[modList[i][0]]:'./mod/'+modList[i][0]+'/script.js').toString();
+            var s=document.createElement('script');
+            document.body.appendChild(s);
+            s.appendChild(document.createTextNode(`try{;${script};}catch(e){alert('error:mod:${modList[i][0]}:'+e.toString());}`));
+        }
+    }
+}catch(e){alert('error:'+e.toString());};
+/*mod_end*/
